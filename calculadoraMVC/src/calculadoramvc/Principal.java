552b52fdf73f3826131adff927bd9a2c0314eeb3
BRUN0RRRR <brunorodriguesbsr@gmail.com>
2024-09-02 19:34:48 -0300

    trabalho em java

---
+++ calculadoraMVC/src/calculadoramvc/Principal.java
@@ -0,0 +1,12 @@
+package calculadoramvc;
+import calculadoramvc.visao.teste2;
+
+public class Principal {
+  public static void main(String [] args){ 
+    System.out.println("executando código"); 
+    teste2 tela = new teste2();
+    tela.setVisible(true);
+}
+
+}
+
---
+++ telaPrincipal/src/telaprincipal/TelaPrincipal.java
@@ -0,0 +1,13 @@
+package telaprincipal;
+
+import javax.swing.JFrame;
+
+public class TelaPrincipal {
+
+    public static void main(String[] args) {
+        // Instancia e exibe a telaCalculadora
+        telaCalculadora tela = new telaCalculadora();
+        tela.setVisible(true);
+        
+    }
+}
